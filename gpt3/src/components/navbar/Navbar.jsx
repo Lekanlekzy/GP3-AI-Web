39cd6514bf94d0a5806c6ca079472ee5352aba60
Lekanlekzy <dauda629@gmail.com>
2024-06-24 23:47:45 -0600

    new

--- gpt3/src/components/navbar/Navbar.jsx
+++ gpt3/src/components/navbar/Navbar.jsx
@@ -9,8 +9,16 @@ const navbar = () => {
         <div className='gpt3-navbar-links_logo'>
           <img src={logo} alt='logo' />
         </div>
+        <div className='gpt3-navbar-links_container'>
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#whatisgpt'>What is GPT?</a></p>
+          <p><a href='#possibility'>Open AI</a></p>
+          <p><a href='#features'>Case Studies</a></p>
+          <p><a href='#blog'>Library</a></p>
+        </div>
       </div>
     </div>
+   
   )
 }
 
