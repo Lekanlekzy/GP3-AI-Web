3a53c38820f92d6a9e2f4af1d4e730ce99307598
Lekanlekzy <dauda629@gmail.com>
2024-07-03 23:27:32 -0600

    navbar

--- gpt3/src/components/navbar/Navbar.jsx
+++ gpt3/src/components/navbar/Navbar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useState}  from 'react'
 import './navbar.css'
 import logo from '../../assets/logo.svg'
+import { RiMenu3Line ,RiCloseLine }  from "react-icons/ri";
 
-const navbar = () => {
+const Menu = ()=> (
+  <>
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#whatisgpt'>What is GPT?</a></p>
+          <p><a href='#possibility'>Open AI</a></p>
+          <p><a href='#features'>Case Studies</a></p>
+          <p><a href='#blog'>Library</a></p>
+  </>
+)
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState("false")
+
   return (
     <div className='gpt3-navbar'>
       <div className='gpt3-navbar-links'>
@@ -10,20 +23,31 @@ const navbar = () => {
           <img src={logo} alt='logo' />
         </div>
         <div className='gpt3-navbar-links_container'>
-          <p><a href='#home'>Home</a></p>
-          <p><a href='#whatisgpt'>What is GPT?</a></p>
-          <p><a href='#possibility'>Open AI</a></p>
-          <p><a href='#features'>Case Studies</a></p>
-          <p><a href='#blog'>Library</a></p>
+          <Menu />
         </div>
       </div>
       <div className='gpt-navbar__sign'>
         <p>Sign in</p>
         <button type='button'> Sign Up</button>
+        <div className='gpt-navbar-menu'>
+          {toggleMenu
+          ? <RiMenu3Line color='#fff' size={27} onClick={()=> setToggleMenu(true)} />
+          : <RiCloseLine color='#fff' size={27} onClick={()=> setToggleMenu(false)} />
+          }
+          {toggleMenu && (
+            <div className='gpt-navbar-menu_container scale-up-center'>
+              <div className='gpt-navbar-menu_container-links'>
+                <Menu />
+              </div>
+            </div>
+          )
+
+          }
+        </div>
       </div>
     </div>
-   
+
   )
 }
 
-export default navbar
+export default Navbar
