800b273bfac693f41cec13932a12952a58875858
Lekanlekzy <dauda629@gmail.com>
2024-07-15 13:42:39 -0600

    gf

--- gpt3/src/containers/index.js
+++ gpt3/src/containers/index.js
@@ -1,5 +1,5 @@
 export { default as Blog } from './blog/Blog'
-export { default as Feature } from './features/Features'
+export { default as Features } from './features/Features'
 export { default as Footers } from './footers/Footers'
 export { default as Header } from './header/Header'
 export { default as Possibility } from './possibility/Possibility'
