c694f92fd8e87d86bcf2927f43b54679c0e760a1
Lekanlekzy <dauda629@gmail.com>
2024-07-16 19:19:29 -0600

    rt

--- gpt3/src/components/brand/Brand.jsx
+++ gpt3/src/components/brand/Brand.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import brand from './brand.css'
+
+import google from '../../assets/google.png'
+import slack from '../../assets/slack.png'
+import Atlassian from '../../assets/atlassian.png'
+import Dropbox from '../../assets/dropbox.png'
+import Shopify from '../../assets/shopify.png'
 const Brand = () => {
   return (
     <div>
-      
+      <img src={google} alt='google'/>
+      <img src={slack} alt='google'/>
+      <img src={Atlassian} alt='google'/>
+      <img src={Dropbox} alt='google'/>
+      <img src={Shopify} alt='google'/>
     </div>
   )
 }
