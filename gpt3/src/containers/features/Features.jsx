62fe45f5ef0bde528e587ee39276fd8fc8cbe962
Lekanlekzy <dauda629@gmail.com>
2024-07-20 23:36:42 -0600

    yt

--- gpt3/src/containers/features/Features.jsx
+++ gpt3/src/containers/features/Features.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './features.css'
-const Features = () => {
+
+const Features = ({title, text}) => {
   return (
     <div className='gpt3-features section__padding' id='features'>
-      <div className='gpt3'>
-
+      <div className='gpt3-features-container__title'>
+        <h1>{title}</h1>
+      </div>
+      <div className='gpt3-features-container__text'>
+        {text}
       </div>
     </div>
   )
