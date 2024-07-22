f32bf830dc046662bd3029aadb20b20744656546
Lekanlekzy <dauda629@gmail.com>
2024-07-21 18:13:03 -0600

    op

--- gpt3/src/containers/features/Features.jsx
+++ gpt3/src/containers/features/Features.jsx
@@ -3,8 +3,9 @@ import './features.css'
 
 const Features = ({title, text}) => {
   return (
-    <div className='gpt3-features section__padding' id='features'>
+    <div className='gpt3-features' id='features'>
       <div className='gpt3-features-container__title'>
+        <div className='gradient__text'/>
         <h1>{title}</h1>
       </div>
       <div className='gpt3-features-container__text'>
