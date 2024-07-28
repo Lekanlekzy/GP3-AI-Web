71f129991a40224e0fba4fe41c4611cf6e2bd6cf
Lekanlekzy <dauda629@gmail.com>
2024-07-27 23:41:51 -0600

    re

--- gpt3/src/components/feature/Feature.jsx
+++ gpt3/src/components/feature/Feature.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import './feature.css'
+import { Feature } from '..'
 
-const featureData = () =>{
+const featureData = [
   {
-    
-  }
-}
-
+    title: 'Improving end distrusts instantly',
+    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.'
+  },
+  {
+    title: 'Become the tended active',
+    text: 'Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.'
+  },
+  {
+    title: 'Message or am nothing',
+    text: 'Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.'
+  },
+  {
+    title: 'Really boy law county',
+    text: 'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.'
+  },
+]
 const feature = () => {
   return (
     <div className="gpt3__features section__padding" id="features">
@@ -15,7 +28,9 @@ const feature = () => {
         <p>Request Early Access to Get Started</p>
       </div>
       <div className='gpt3__features-container'>
-
+        {featureData.map((item, index) => callback(item, index)
+        (<Feature />))
+        }
       </div>
     </div>
   )
