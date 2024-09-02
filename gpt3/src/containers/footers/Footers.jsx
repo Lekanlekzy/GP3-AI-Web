a493fd32a9693f0e7059a00ae0c3e7504280aae6
Lekanlekzy <dauda629@gmail.com>
2024-09-01 22:48:31 -0600

    footer

--- gpt3/src/containers/footers/Footers.jsx
+++ gpt3/src/containers/footers/Footers.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import './footer.css'
 const Footers = () => {
   return (
-    <div>
-      <div>
-
+    <div className='gpt3__footer' >
+      <div className='gpt3__footer-content'>
+        <h1 className=' gradient__text'>Do you want to step in to the future before others</h1>
+        <button>Request Early Access</button>
       </div>
       <div>
-        
+
       </div>
     </div>
   )
